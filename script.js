083b8c15d80bded5a183104918f5820bfb3032c9
Felipe Noronha <felipe.dariva07@gmail.com>
2024-10-15 08:29:15 -0300

    projeto feira tecnico v1.1.3
    comentários adicionados

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-let dx;
-let dy;
-let px;
-let py;
-let vel;
-let tmp;
-let obj;
+let dx; //direção do eixo x
+let dy; //direção do eixo y
+let px; //posição final do eixo x
+let py; //posição final do eixo y
+let vel; //velocidade
+let tmp; //intervalo de tempo até executar de novo a função
+let obj; //objeto/personagem
 
+//inicia as variáveis
 function inicia () {
     dx = 0;
     dy = 0;
     px = 0;
     py = 0;
-    vel = 1;
-    tmp = setInterval (tempo, 1);
+    vel = 1.5;
+    tmp = setInterval (funcionalidades, 1);
     obj = document.getElementById('object');
     document.addEventListener('keydown', teclaBaixo);
     document.addEventListener('keyup', teclaCima);
 };
 
+//movimenta o personagem
 function teclaBaixo (event) {
     let tecla = event.keyCode;
     if (tecla == 65) {
@@ -31,6 +33,7 @@ function teclaBaixo (event) {
     };
 };
 
+//para de movimentar
 function teclaCima (event) {
     let tecla = event.keyCode;
     if (tecla == 65) {
@@ -44,7 +47,8 @@ function teclaCima (event) {
     };
 };
 
-function tempo () {
+//declara a velocidade do personagem e outras funionalidades
+function funcionalidades () {
     px += dx * vel;
     py += dy * vel;
 
