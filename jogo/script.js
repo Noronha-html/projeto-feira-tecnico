578ac4529fb91b3a0fb9416d46aebba89b15663a
Felipe Noronha <felipe.dariva07@gmail.com>
2024-10-17 09:19:12 -0300

    projeto-feira-tecnico v0.2.2
    parando quando tem parede!

--- jogo/script.js
+++ jogo/script.js
@@ -3,9 +3,15 @@ let dy; //direção do eixo y
 let px; //posição final do eixo x
 let py; //posição final do eixo y
 let vel; //velocidade
-let tmp; //intervalo de tempo até executar de novo a função
-let objeto1; //objeto1/personagem1
-let objeto2; //objeto2/personagem2
+let tmpFuncionalidades; //intervalo de tempo até executar de novo a função
+let tmpDetectarColisao; //intervalo de tempo até executar de novo a função
+let objetoMario; //objeto1/personagem1
+let paredeD; //parede da direita
+let paredeE; //parede da esquerda
+let tecA = true; //declara a tecla A como verdadeira
+let tecD = true; //declara a tecla D como verdadeira
+let tecW = true; //declara a tecla W como verdadeira
+let tecS = true; //declara a tecla S como verdadeira
 
 //inicia as variáveis
 function inicia () {
@@ -14,24 +20,26 @@ function inicia () {
     px = 0;
     py = 0;
     vel = 1.5;
-    tmp = setInterval (funcionalidades, 1);
-    objeto2 = document.getElementById('mario');
+    tmpFuncionalidades = setInterval (funcionalidades, 1);
+    tmpDetectarColisao = setInterval (detectarColisao, 1)
+    objetoMario = document.getElementById('mario');
     document.addEventListener('keydown', teclaBaixo);
     document.addEventListener('keyup', teclaCima);
 
-    objeto1 = document.getElementById('cogumelo');
+    paredeD = document.getElementById('paredeD');
+    paredeE = document.getElementById('parede E')
 };
 
 //movimenta o personagem
 function teclaBaixo (event) {
     let tecla = event.keyCode;
-    if (tecla == 65) {
+    if (tecla == 65 && tecA) {
         dx = -1;
-    } else if (tecla == 68) {
+    } else if (tecla == 68 && tecD) {
         dx = 1;
-    } else if (tecla == 87) {
+    } else if (tecla == 87 && tecW) {
         dy = -1;
-    } else if (tecla == 83) {
+    } else if (tecla == 83 && tecS) {
         dy = 1;
     };
 };
@@ -55,36 +63,54 @@ function funcionalidades () {
     px += dx * vel;
     py += dy * vel;
 
-    objeto2.style.left = px + 'px';
-    objeto2.style.top = py + 'px';
-
-    (detectarColisao('cogumelo', 'mario') == true)?console.log('colidiu') : console.log('ainda não colidiu');
+    objetoMario.style.left = px + 'px';
+    objetoMario.style.top = py + 'px';
+    console.log(px);
+    console.log(py);
+    (detectarColisao('paredeD', 'mario') == true)?console.log('colidiu') : console.log('ainda não colidiu');
 };
 
 function detectarColisao(idObjeto2, idObjeto1) {
-    let objeto1 = document.getElementById(idObjeto1).getBoundingClientRect();
-    let objeto2 = document.getElementById(idObjeto2).getBoundingClientRect();
+    let objetoMario = document.getElementById(idObjeto1).getBoundingClientRect();
+    let paredeD = document.getElementById(idObjeto2).getBoundingClientRect();
 
-    let pontos_obj1 = [{x : objeto1.left, y : objeto1.top}, 
-                        {x : objeto1.left + objeto1.width, y : objeto1.top},
-                        {x : objeto1.left + objeto1.width, y : objeto1.top + objeto1.height},
-                        {x : objeto1.left, y : objeto1.top + objeto1.height}];
+    let pontos_mario = [{x : objetoMario.left, y : objetoMario.top}, 
+                        {x : objetoMario.left + objetoMario.width, y : objetoMario.top},
+                        {x : objetoMario.left + objetoMario.width, y : objetoMario.top + objetoMario.height},
+                        {x : objetoMario.left, y : objetoMario.top + objetoMario.height}];
 
-    let pontos_obj2 = [{x : objeto2.left, y : objeto2.top}, 
-                        {x : objeto2.left + objeto2.width, y : objeto2.top},
-                        {x : objeto2.left + objeto2.width, y : objeto2.top + objeto2.height},
-                        {x : objeto2.left, y : objeto2.top + objeto2.height}];
+    let pontos_parede_D = [{x : paredeD.left, y : paredeD.top}, 
+                        {x : paredeD.left + paredeD.width, y : paredeD.top},
+                        {x : paredeD.left + paredeD.width, y : paredeD.top + paredeD.height},
+                        {x : paredeD.left, y : paredeD.top + paredeD.height}];
 
     let indice = 0;
     let colidiu = false;
 
     while ((colidiu == false) && (indice < 3))
-        ((pontos_obj1[indice].x >= objeto2.left && pontos_obj1[indice].x <= objeto2.left + objeto2.width && 
-        pontos_obj1[indice].y >= objeto2.top && pontos_obj1[indice].y <= objeto2.top + objeto2.height)) ||
+        ((pontos_mario[indice].x >= paredeD.left && pontos_mario[indice].x <= paredeD.left + paredeD.width && 
+        pontos_mario[indice].y >= paredeD.top && pontos_mario[indice].y <= paredeD.top + paredeD.height)) ||
 
-        ((pontos_obj2[indice].x >= objeto1.left && pontos_obj2[indice].x <= objeto1.left + objeto1.width && 
-        pontos_obj2[indice].y >= objeto1.top && pontos_obj2[indice].y <= objeto1.top + objeto1.height)) ? colidiu = true : indice ++;
+        ((pontos_parede_D[indice].x >= objetoMario.left && pontos_parede_D[indice].x <= objetoMario.left + objetoMario.width && 
+        pontos_parede_D[indice].y >= objetoMario.top && pontos_parede_D[indice].y <= objetoMario.top + objetoMario.height)) 
+        ? colidiu = true : indice ++;
+    tecD = false
+
+    if (colidiu == true && tecD == false) {
+        dx = 0
+    } else {
+        tecD = true
+    }
+
     return colidiu;
+
+    while ((colidiu == false) && (indice < 3))
+    ((pontos_mario[indice].x >= paredeD.left && pontos_mario[indice].x <= paredeD.left + paredeD.width && 
+        pontos_mario[indice].y >= paredeD.top && pontos_mario[indice].y <= paredeD.top + paredeD.height)) ||
+
+        ((pontos_parede_D[indice].x >= objetoMario.left && pontos_parede_D[indice].x <= objetoMario.left + objetoMario.width && 
+        pontos_parede_D[indice].y >= objetoMario.top && pontos_parede_D[indice].y <= objetoMario.top + objetoMario.height)) 
+        ? colidiu = true : indice ++;
 };
 
 window.addEventListener('load', inicia);
