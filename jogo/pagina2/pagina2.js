605ee7e2dcda17fbd6d3b58a22b01f7eb4f11c26
Arthur <60153157097@senacrs.edu.br>
2024-11-26 12:22:22 -0300

    indev v0.7.2
    fazendo as salas do level desinign

--- jogo/pagina2/pagina2.js
+++ jogo/pagina2/pagina2.js
@@ -125,7 +125,9 @@ function inicia () {
     quadLeft = document.getElementById('quadLeft');
     quadRight = document.getElementById('quadRight');
 
-    portaDown = document.getElementById('portaDown');
+    //portaDown = document.getElementById('portaDown');
+    portaRight = document.getElementById('portaRight');
+    portaLeft = document.getElementById('portaLeft');
 
     boss_cultista_wrapper = document.getElementById('boss_cultista-wrapper')
     boss_cultista = document.getElementById('boss_cultista');
@@ -316,7 +318,9 @@ function funcionalidades () {
     detectarColisaoBossBottom__ParedeB__quadTop('boss_cultista-bottom', 'paredeB', 'quadTop');*/
 
     //detectarColisaoPortaTop('player', 'portaTop');
-    detectarColisaoPortaDown('player', 'portaDown');
+    //detectarColisaoPortaDown('player', 'portaDown');
+    detectarColisaoPortaRight('player', 'portaRight');
+    detectarColisaoPortaLeft('player', 'portaLeft');
 
     playerAttackLeft('player');
     playerAttackRight('player');
@@ -765,7 +769,7 @@ function detectarColisaoBossRight__ParedeD__quadLeft(idObjeto1, idObjeto2, idObj
 }
 
 //detectar colisões nas portas
-function detectarColisaoPortaDown (idObjeto1, idObjeto2) {
+/*function detectarColisaoPortaDown (idObjeto1, idObjeto2) {
     let objetoPlayer = document.getElementById(idObjeto1).getBoundingClientRect();
     let portaDown = document.getElementById(idObjeto2).getBoundingClientRect();
 
@@ -795,7 +799,72 @@ function detectarColisaoPortaDown (idObjeto1, idObjeto2) {
     }
 
     return colidiu;
+}*/
+
+function detectarColisaoPortaRight (idObjeto1, idObjeto2) {
+    let objetoPlayer = document.getElementById(idObjeto1).getBoundingClientRect();
+    let portaRight = document.getElementById(idObjeto2).getBoundingClientRect();
+    let pontos_Player = [{x : objetoPlayer.left, y : objetoPlayer.top}, 
+                         {x : objetoPlayer.left + objetoPlayer.width, y : objetoPlayer.top},
+                         {x : objetoPlayer.left + objetoPlayer.width, y : objetoPlayer.top + objetoPlayer.height},
+                         {x : objetoPlayer.left, y : objetoPlayer.top + objetoPlayer.height}];
+
+    let pontos_porta_Right = [{x : portaRight.left, y : portaRight.top}, 
+                           {x : portaRight.left + portaRight.width, y : portaRight.top},
+                           {x : portaRight.left + portaRight.width, y : portaRight.top + portaRight.height},
+                           {x : portaRight.left, y : portaRight.top + portaRight.height}];
+
+    indice = 0;
+    colidiu = false
+
+    while ((colidiu == false) && (indice < 3))
+    ((pontos_Player[indice].x >= portaRight.left && pontos_Player[indice].x <= portaRight.left + portaRight.width && 
+    pontos_Player[indice].y >= portaRight.top && pontos_Player[indice].y <= portaRight.top + portaRight.height)) ||
+
+    ((pontos_porta_Right[indice].x >= objetoPlayer.left && pontos_porta_Right[indice].x <= objetoPlayer.left + objetoPlayer.width && 
+    pontos_porta_Right[indice].y >= objetoPlayer.top && pontos_porta_Right[indice].y <= objetoPlayer.top + objetoPlayer.height))
+    ? colidiu = true : indice ++;
+    
+    if (colidiu == true) {
+        window.location.href = '/jogo/pagina3/pagina3.html';
+    }
+
+    return colidiu;
 }
+
+
+function detectarColisaoPortaLeft (idObjeto1, idObjeto2) {
+    let objetoPlayer = document.getElementById(idObjeto1).getBoundingClientRect();
+    let portaLeft = document.getElementById(idObjeto2).getBoundingClientRect();
+
+    let pontos_Player = [{x : objetoPlayer.left, y : objetoPlayer.top}, 
+                         {x : objetoPlayer.left + objetoPlayer.width, y : objetoPlayer.top},
+                         {x : objetoPlayer.left + objetoPlayer.width, y : objetoPlayer.top + objetoPlayer.height},
+                         {x : objetoPlayer.left, y : objetoPlayer.top + objetoPlayer.height}];
+
+    let pontos_porta_Left = [{x : portaLeft.left, y : portaLeft.top}, 
+                           {x : portaLeft.left + portaLeft.width, y : portaLeft.top},
+                           {x : portaLeft.left + portaLeft.width, y : portaLeft.top + portaLeft.height},
+                           {x : portaLeft.left, y : portaLeft.top + portaLeft.height}];
+
+    indice = 0;
+    colidiu = false
+
+    while ((colidiu == false) && (indice < 3))
+    ((pontos_Player[indice].x >= portaLeft.left && pontos_Player[indice].x <= portaLeft.left + portaLeft.width && 
+    pontos_Player[indice].y >= portaLeft.top && pontos_Player[indice].y <= portaLeft.top + portaLeft.height)) ||
+
+    ((pontos_porta_Left[indice].x >= objetoPlayer.left && pontos_porta_Left[indice].x <= objetoPlayer.left + objetoPlayer.width && 
+    pontos_porta_Left[indice].y >= objetoPlayer.top && pontos_porta_Left[indice].y <= objetoPlayer.top + objetoPlayer.height))
+    ? colidiu = true : indice ++;
+    
+    if (colidiu == true) {
+        window.location.href = '/jogo/pagina1/index.html';
+    }
+
+    return colidiu;
+}
+
 
 //ataque do personagem
 function atacar(event) {
